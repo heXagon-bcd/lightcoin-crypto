9ed5c666250c1039e34c1940c1d444676489930e
Shawn He <shawnhe1991@gmail.com>
2023-03-16 21:36:22 -0400

    code complete before stretch

--- index.js
+++ index.js
@@ -7,22 +7,24 @@ class Transaction {
     this.account = account;
   }
 
-}
-
-class Deposit extends Transaction {
-
   commit() {
-    this.account.balance += this.amount;//creates object to access
-    console.log(this.account);
+    // Keep track of the time of the transaction
+    this.time = new Date();
+    // Add the transaction to the account
+    this.account.addTransaction(this);
+    this.account.balance += this.value;// value is called from deposit
   }
-
+}
+class Deposit extends Transaction {
+  get value() {
+    return this.amount
+  }
 }
 
 class Withdrawal extends Transaction {
 
-  commit() {
-    this.account.balance -= this.amount;
-    console.log(this.account);
+  get value() {
+    return -this.amount
   }
 
 }
@@ -32,15 +34,35 @@ class Account {
   this.username = username;
   //balance
   this.balance = 0;
+  //transactions
+  this.transactions = [];
   }
 
+  // get balance {
+  // return this.Account.balance()
+  // }
 
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
 
 }
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-const myAccount = new Account("snow-patrol");
-t1 = new Withdrawal(50.25, myAccount);
+// const myAccount = new Account("snow-patrol");
+// t1 = new Withdrawal(50.25, myAccount);
+// t1.value();
+// console.log(myAccount.balance)
+
+const myAccount = new Account('billybob');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log(myAccount.balance)
+
+const t2 = new Withdrawal(50.00, myAccount);
+t2.commit();
+//console.log(myAccount)
+console.log('Ending Balance:', myAccount.balance);
